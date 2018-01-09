e57a3ead72b6253c6b35a2fc165053a5b569e650
Adam M Deeley <ad.deeley@gmail.com>
2018-01-09 21:42:27 +0000

    Added circles that represent number of books written horisontally.

--- js/src/index.js
+++ js/src/index.js
@@ -1,8 +1,25 @@
 window.onload = function() {
-    let content = document.createElement("div"),
-        para = document.createElement("p"),
-        body = document.getElementsByTagName("body")[0];
+    d3.json('data/authors.json', function(data) {
+        let margin = {top: 20, right: 20, 
+                      bottom: 50, left: 70}
 
-    para.innerHTML = "Hello, World!",
-    body.appendChild(content.appendChild(para));
+        let radius = 10,
+            width = 800,
+            height = 600,
+            outerWidth = width + margin.right + margin.left,
+            outerHeight = height + margin.top + margin.bottom,
+            n = null;
+
+        let chart = d3.select('.chart')
+            .attr('width', outerWidth)
+            .attr('height', outerHeight)
+
+        let circle = chart.selectAll('circle')
+            .data(data)
+        .enter().append('circle')
+            .attr('class', 'point')
+            .attr('cx', (d) => parseInt(d[5]))
+            .attr('cy', radius)
+            .attr('r', radius)
+    })
 }
